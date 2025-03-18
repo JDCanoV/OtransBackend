8ecf05fc34cce40626803cf9a9d99b4b482151e2
ospaternina <oscardadia3020@hotmail.com>
2025-03-18 11:29:11 -0500

    Utilities-services
    creacion de carpeta

--- OtransBackend/OtransBackend/obj/Debug/net8.0/OtransBackend.AssemblyInfo.cs
+++ OtransBackend/OtransBackend/obj/Debug/net8.0/OtransBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtransBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364484d479024cbb85fc22a674bacc30359ce52b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ef77176d3af3f1cbfbeef72a11f3303b87319e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtransBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtransBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
